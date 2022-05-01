8bced5ec29277856a2d20bba11793aec92c91f5b
Anuj <51194560+AnujDagade@users.noreply.github.com>
2022-05-01 18:04:33 +0530

    Delete deci.py

--- deci.py
+++
@@ -1,9 +0,0 @@
-class GPIO_Handler:
-    def match(msg):
-        if(msg.lower() == 'pratik'):
-            print('HI',msg)
-            s = 'HI '+msg
-            return s
-        else:
-            print('Wrong Command')
-            return 'Wrong Command'
